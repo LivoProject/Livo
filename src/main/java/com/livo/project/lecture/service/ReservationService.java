c450717295680dc3cdb43642fe37d328cc8dd743
alsdud4210-test <alsdud4210@gmail.com>
2025-10-20 14:00:20 +0900

    민영 리뷰신고까지 완료

--- src/main/java/com/livo/project/lecture/service/ReservationService.java
+++ src/main/java/com/livo/project/lecture/service/ReservationService.java
@@ -5,4 +5,7 @@ public interface ReservationService {
 
     //로그인 유저가 특정 강의 수강 중인지
     boolean isUserEnrolled(int lectureId, String email);
+
+    // 리뷰 등록 시 reservationId 가져오기 (ReviewController)
+    Integer findReservationIdByEmailAndLectureId(String email, int lectureId);
 }
