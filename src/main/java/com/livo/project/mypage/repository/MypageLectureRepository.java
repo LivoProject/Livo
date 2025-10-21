b1efe4625b324a359633c8fa2ad46ae2811a4e1f
subin <sooxpak@gmail.com>
2025-10-22 00:08:46 +0900

    마이페이지 좋아요강의 페이징

--- src/main/java/com/livo/project/mypage/repository/MypageLectureRepository.java
+++ src/main/java/com/livo/project/mypage/repository/MypageLectureRepository.java
@@ -2,6 +2,8 @@ package com.livo.project.mypage.repository;
 
 import com.livo.project.lecture.domain.Lecture;
 import jakarta.transaction.Transactional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -17,23 +19,30 @@ public interface MypageLectureRepository extends JpaRepository<Lecture, Integer>
 
     // 좋아요한 강좌
     @Query(value = """
-    SELECT l.* 
-    FROM lecture l
-    JOIN lecture_like ll ON l.lectureId = ll.lectureId
-    WHERE ll.email = :email
-    ORDER BY ll.createdAt DESC
-    """, nativeQuery = true)
-    List<Lecture> findLikedLecturesByEmail(String email);
+             SELECT l.* 
+            FROM lecture l
+            JOIN lecture_like ll ON l.lectureId = ll.lectureId
+            WHERE ll.email = :email
+            ORDER BY ll.createdAt DESC
+            """,
+            countQuery = """
+                    SELECT COUNT(*) 
+                    FROM lecture l
+                    JOIN lecture_like ll ON l.lectureId = ll.lectureId
+                    WHERE ll.email = :email
+                    """,
+            nativeQuery = true)
+    Page<Lecture> findLikedLecturesByEmail(@Param("email") String email, Pageable pageable);
 
     // 좋아요한 강좌 최신 2개
     @Query(value = """
-    SELECT l.* 
-    FROM lecture l
-    JOIN lecture_like ll ON l.lectureId = ll.lectureId
-    WHERE ll.email = :email
-    ORDER BY ll.createdAt DESC
-    LIMIT 2
-    """, nativeQuery = true)
+            SELECT l.* 
+            FROM lecture l
+            JOIN lecture_like ll ON l.lectureId = ll.lectureId
+            WHERE ll.email = :email
+            ORDER BY ll.createdAt DESC
+            LIMIT 2
+            """, nativeQuery = true)
     List<Lecture> findTop2LikedLecturesByEmail(@Param("email") String email);
 
     // 좋아요 해제
