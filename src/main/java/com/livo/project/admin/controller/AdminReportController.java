5ae96fb29275b4bc90ae25d1cf576418b64b4ffb
dusgml4258-test <dusgml4258@gmail.com>
2025-10-22 17:56:43 +0900

    신고 리스트 불러오기

--- src/main/java/com/livo/project/admin/controller/AdminReportController.java
+++ src/main/java/com/livo/project/admin/controller/AdminReportController.java
@@ -1,19 +1,53 @@
 package com.livo.project.admin.controller;
 
+import com.livo.project.report.domain.Report;
 import com.livo.project.report.service.ReportService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/admin/report")
 public class AdminReportController {
     private final ReportService reportService;
+
+    @GetMapping("/list")
+    @ResponseBody
+    public Page<Report> getReportList(@RequestParam(defaultValue = "0")int page,
+                                      @RequestParam(defaultValue = "10")int pageSize){
+        Pageable pageable = PageRequest.of(page,pageSize, Sort.by(Sort.Direction.DESC,"reportTime"));
+        return reportService.getReport(pageable);
+    }
+    @PostMapping("/approve/{reportId}")
+    public ResponseEntity<?> apprveReport(@PathVariable("reportId") int reportId){
+        try{
+            reportService.approveReport(reportId);
+            return ResponseEntity.ok("신고 승인 완료(해당 리뷰 삭제됨)");
+        }catch (Exception e){
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("승인 실패: "+e.getMessage());
+        }
+    }
+
+    @PostMapping("/reject/{reportId}")
+    public ResponseEntity<?> rejectReport(@PathVariable("reportId") int reportId){
+        try{
+            reportService.rejectReport(reportId);
+            return ResponseEntity.ok("신고 거부 완료");
+        }catch (Exception e){
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("거부 실패: "+e.getMessage());
+        }
+    }
 
 
 }
--- src/main/java/com/livo/project/report/service/ReportServiceImpl.java
+++ src/main/java/com/livo/project/report/service/ReportServiceImpl.java
@@ -2,17 +2,47 @@ package com.livo.project.report.service;
 
 import com.livo.project.report.domain.Report;
 import com.livo.project.report.repository.ReportRepository;
+import com.livo.project.review.repository.ReviewRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+@Transactional
 @Service
 @RequiredArgsConstructor
 public class ReportServiceImpl implements ReportService {
 
     private final ReportRepository reportRepository;
-
+    private final ReviewRepository reviewRepository;
     @Override
     public Report saveReport(Report report) {
         return reportRepository.save(report);
+    }
+
+    @Override
+    public void approveReport(int reportId) {
+        Report report = reportRepository.findById(reportId)
+                .orElseThrow(() ->new IllegalArgumentException("해당 신고가 존재 하지 않습니다."));
+        report.setStatus(Report.Status.COMPLETED);
+        reportRepository.save(report);
+
+        reviewRepository.deleteById(report.getReviewUId());
+    }
+
+    @Override
+    public void rejectReport(int reportId) {
+        Report report = reportRepository.findById(reportId)
+                .orElseThrow(() ->new IllegalArgumentException("해당 신고가 존재 하지 않습니다."));
+        report.setStatus(Report.Status.REJECT);
+        reportRepository.save(report);
+    }
+
+    @Override
+    public Page<Report> getReport(Pageable pageable) {
+        return reportRepository.findAll(pageable);
     }
 }
