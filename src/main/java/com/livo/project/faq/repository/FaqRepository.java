02085402f419f793cddc874fb32259bf931cf84c
dusgml4258-test <dusgml4258@gmail.com>
2025-10-27 11:42:06 +0900

    faq 수정 기능

--- src/main/java/com/livo/project/faq/repository/FaqRepository.java
+++ src/main/java/com/livo/project/faq/repository/FaqRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface FaqRepository extends JpaRepository<Faq, String> {
+public interface FaqRepository extends JpaRepository<Faq, Integer> {
     List<Faq> findTop5ByOrderByCreatedAtDesc();
 }
